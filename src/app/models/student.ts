04f1555d585fdc7a2aec491c8d148864fb70ead3
BHUPSAHU <bhupesh.sahu@capgemini.com>
2021-09-06 18:12:34 +0530

    Student Basic CRUD working

---
+++ src/app/models/student.ts
@@ -0,0 +1,18 @@
+export class Student {
+    constructor(
+        public studentId:number,
+        public rollNo:number,
+        public firstName:string,
+        public lastName:string,
+        public dob:Date,
+        public gender:string,
+        public mobileNo:string,
+        public courseId:number,
+        public courseName:string,
+        public emailId:string,
+        public fatherMobileNo:string,
+        public fatherEmailId:string,
+        public profilePic:string,
+        public name:string
+    ){}
+}
--- src/app/student/student-update/student-update.component.ts
+++ src/app/student/student-update/student-update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Student } from 'src/app/models/student';
+import { HttpStudentClientService } from 'src/app/services/http-student-client.service';
 
 @Component({
   selector: 'app-student-update',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-update.component.css']
 })
 export class StudentUpdateComponent implements OnInit {
-
-  constructor() { }
+id:number = 0 ;
+submitted:boolean =false;
+student:Student = new Student(0,0,"","",new Date,"","",0,"","","","","","");
+studentObs : Observable<Student> = new Observable<Student>();
+sid:string =''
+  constructor(private httpClientService:HttpStudentClientService,private route: ActivatedRoute,private router:Router) { }
 
   ngOnInit(): void {
+    this.sid = this.route.snapshot.params['id'];
+    this.id = Number.parseInt(this.sid);
+    this.student =new Student(this.id,0,"","",new Date,"","",0,"","","","","",""); 
+    this.httpClientService.getStudentById(this.id).subscribe(data =>{
+      this.student=data;
+    },error => console.log(error));
+  }
+
+  onSubmit() {
+    console.log("update")
+    this.submitted = true;
+    this.studentObs =this.httpClientService.updateStudent(this.student);
+    this.studentObs.subscribe(data =>{
+      alert("Student Updated Successfully.")
+    })
+    this.router.navigate(['student/list']);
+  } 
+
+  list(){
+    this.router.navigate(['student/list']);
   }
 
 }
